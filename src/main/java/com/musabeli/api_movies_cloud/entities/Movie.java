d580cf74187a8eb66dde7279609a9cced488a7ae
Albania Musabeli <musabelialbania@gmail.com>
2025-04-05 16:19:02 -0300

    feat: initial commit - API Movies CRUD Oracle Cloud
    - Configurar dependencias para conexión a Oracle Cloud
    - Configurar entidad Movie

---
+++ src/main/java/com/musabeli/api_movies_cloud/entities/Movie.java
@@ -0,0 +1,34 @@
+package com.musabeli.api_movies_cloud.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private String director;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private String synopsis;
+
+}
---
+++ src/test/java/com/musabeli/api_movies_cloud/ApiMoviesCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musabeli.api_movies_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
